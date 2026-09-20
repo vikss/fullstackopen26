c7db5e4feb88d22007a52d4bb602089f015680a5
vikss <viraktijain01@gmail.com>
2026-09-20 15:52:29 +0530

    frotend plus backend changes

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 
 
-const cors = require('cors')
+
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 morgan.token('content', (req, res)=>{
 
 
